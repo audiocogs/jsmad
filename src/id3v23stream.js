85d66c015ee54f2c2df02b95382f31beb8adc6fc
Amos Wenger <amos@official.fm>
2011-11-27 17:34:46 +0100

    Reformatted a few files

--- src/id3v23stream.js
+++ src/id3v23stream.js
@@ -1,5 +1,5 @@
 var trimString = function (string) {
-  return string.replace(/\0$/, "");
+    return string.replace(/\0$/, "");
 };
 
 var stringToEncoding = function(string, encoding) {
@@ -8,35 +8,35 @@ var stringToEncoding = function(string, encoding) {
             return trimString(string);
         case 1:
             var ix = 2, offset1 = 1, offset2 = 0;
-            
+
             if (string.slice(0, 2) === "\xFE\xFF") {
                 offset1 = 0, offset2 = 1;
             } else {
                 offset1 = 1, offset2 = 0;
             }
-            
+
             var result = "";
-            
+
             for (var ix = 2; ix < string.length; ix += 2) {
                 var byte1 = string[ix + offset1].charCodeAt(0);
                 var byte2 = string[ix + offset2].charCodeAt(0);
-                
+
                 var word1 = (byte1 << 8) | byte2;
-                
+
                 if (byte1 < 0xD8 || byte1 >= 0xE0) {
                     result += String.fromCharCode(word1);
                 } else {
                     ix += 2;
-                    
+
                     var byte3 = string[ix + offset1].charCodeAt(0);
                     var byte4 = string[ix + offset2].charCodeAt(0);
-                    
+
                     var word2 = (byte3 << 8) | byte4;
-                    
+
                     result += String.fromCharCode(word1, word2);
                 }
             }
-            
+
             return trimString(result);
         default:
             return string;
@@ -45,95 +45,95 @@ var stringToEncoding = function(string, encoding) {
 
 var decodeTerminatedString = function(header, stream) {
 
-	// skip text encoding (nobody cares, always ISO-8559-15)
-	stream.readAsString(1);
+    // skip text encoding (nobody cares, always ISO-8559-15)
+    stream.readAsString(1);
 
-	var c;
-	
-	var mimetype = "";
-	while(true) {
-		c = stream.readAsString(1);
-		if(c === "\0") {
-			break;
-		}
-		mimetype += c;
-	}
-	console.log("mimetype = " + mimetype);
-	
-	var filename = "";
-	while(true) {
-		c = stream.readAsString(1);
-		if(c === "\0") {
-			break;
-		}
-		filename += c;
-	}
-	console.log("filename = " + filename);
-	
-	var contentDescription = "";
-	while(true) {
-		c = stream.readAsString(1);
-		if(c === "\0") {
-			break;
-		}
-		contentDescription += c;
-	}
-	console.log("contentDescription = " + contentDescription);
-	
-	var objectName = stream.readAsString(4);
-	
-	// skip 4 bytes, then read binary length
-	stream.readAsString(4);
-	var length = stream.readU32(true) - 4;
-	
-	if(length > header.length) return null;
-	var value = stream.readAsString(length);
-	
-	return {
-		'header': header,
-		'mimetype': mimetype,
-		'filename': filename,
-		'contentDescription': contentDescription,
-		'objectName': objectName,
-		'value': value
-	};
+    var c;
+
+    var mimetype = "";
+    while(true) {
+        c = stream.readAsString(1);
+        if(c === "\0") {
+            break;
+        }
+        mimetype += c;
+    }
+    console.log("mimetype = " + mimetype);
+
+    var filename = "";
+    while(true) {
+        c = stream.readAsString(1);
+        if(c === "\0") {
+            break;
+        }
+        filename += c;
+    }
+    console.log("filename = " + filename);
+
+    var contentDescription = "";
+    while(true) {
+        c = stream.readAsString(1);
+        if(c === "\0") {
+            break;
+        }
+        contentDescription += c;
+    }
+    console.log("contentDescription = " + contentDescription);
+
+    var objectName = stream.readAsString(4);
+
+    // skip 4 bytes, then read binary length
+    stream.readAsString(4);
+    var length = stream.readU32(true) - 4;
+
+    if(length > header.length) return null;
+    var value = stream.readAsString(length);
+
+    return {
+        'header': header,
+            'mimetype': mimetype,
+            'filename': filename,
+            'contentDescription': contentDescription,
+            'objectName': objectName,
+            'value': value
+    };
 }
 
 var decodeTextFrame = function(header, stream) {
     var encoding = stream.readU8();
     var data = stream.readAsString(header['length'] - 1);
-    
+
     return {
         'header': header,
-        'value': stringToEncoding(data, encoding)
+            'value': stringToEncoding(data, encoding)
     };
 }
 
 var decodeAttachedPictureFrame = function(header, stream) {
     var encoding = stream.readU8();
-    
+
     var data = stream.readAsString(header['length'] - 1);
-    
+
     var array = data.split("\0");
-    
+
     return {
         'header': header,
-        'mime': stringToEncoding(array[0], 0),
-        'type': array[1].charCodeAt(0),
-        'description': array[1].slice(1, array[1].length - 1),
-        'value': array.slice(2, array.length - 2).join("\0")
+            'mime': stringToEncoding(array[0], 0),
+            'type': array[1].charCodeAt(0),
+            'description': array[1].slice(1, array[1].length - 1),
+            'value': array.slice(2, array.length - 2).join("\0")
     };
 }
 
 var decodeIdentifierFrame = function(header, stream) {
     var data = stream.readAsString(header['length']);
-    
+
     var array = data.split("\0", 2);
-    
+
     return {
         'header': header,
-        'owner': array[0],
-        'identifier': array[1]
+            'owner': array[0],
+            'identifier': array[1]
     };
 }
 
@@ -141,44 +141,44 @@ var decodeIdentifierFrame = function(header, stream) {
 var decodeCommentFrame = function(header, stream) {
     var encoding = stream.readU8();
     var language = stream.readAsString(3);
-    
+
     var data = stream.readAsString(header['length'] - 4);
-    
+
     var array = data.split("\0", 2);
-    
+
     return {
         'header': header,
-        'language': stringToEncoding(language, 0),
-        'description': stringToEncoding(array[0], 0),
-        'value': stringToEncoding(array[1], encoding)
+            'language': stringToEncoding(language, 0),
+            'description': stringToEncoding(array[0], 0),
+            'value': stringToEncoding(array[1], encoding)
     };
 }
 
 var decodeBinaryFrame = function(header, stream) {
     var data = stream.readAsString(header['length']);
-    
+
     return {
         'header': header,
-        'value': data
+            'value': data
     };
 }
 
 var decodeUserDefinedLinkFrame = function(header, stream) {
     var encoding = stream.readU8();
     var data = stream.readAsString(header['length'] - 1);
-    
+
     var array = data.split("\0", 2);
-    
+
     return {
         'header': header,
-        'description': stringToEncoding(array[0], 0),
-        'value': stringToEncoding(array[1], encoding)
+            'description': stringToEncoding(array[0], 0),
+            'value': stringToEncoding(array[1], encoding)
     };
 }
 
 var extendWithValueFrame = function(tags, frame) {
     tags[frame['name']] = frame['value'];
-    
+
     return tags;
 }
 
@@ -186,14 +186,14 @@ var extendWithPictureFrame = function(tags, frame) {
     if (!tags[frame['name']]) {
         tags[frame['name']] = []
     }
-    
+
     tags[frame['name']].push({
-        'mime': frame['mime'],
-        'type': frame['type'],
-        'description': frame['description'],
-        'value': frame['value']
-    });
-    
+            'mime': frame['mime'],
+            'type': frame['type'],
+            'description': frame['description'],
+            'value': frame['value']
+            });
+
     return tags;
 }
 
@@ -202,7 +202,7 @@ var extendWithIdentifierFrame = function(tags, frame) {
         'owner': frame['value'],
         'identifier': frame['identifier']
     };
-    
+
     return tags;
 }
 
@@ -210,12 +210,12 @@ var extendWithCommentFrame = function(tags, frame) {
     if (!tags[frame['name']]) {
         tags[frame['name']] = []
     }
-    
+
     tags[frame['name']][frame['description']] = {
         'language': frame['language'],
         'value': frame['value']
     };
-    
+
     return tags;
 }
 
@@ -223,9 +223,9 @@ var extendWithUserDefinedLinkFrame = function(tags, frame) {
     if (!tags[frame['name']]) {
         tags[frame['name']] = {}
     }
-    
+
     tags[frame['name']][frame['description']] = frame['value'];
-    
+
     return tags;
 }
 
@@ -233,18 +233,18 @@ var extendWithPrivateFrame = function(tags, frame) {
     if (!tags[frame['name']]) {
         tags[frame['name']] = []
     }
-    
+
     tags[frame['name']].push(frame['value']);
-    
+
     return tags;
 }
 
 Mad.ID3v23Stream = function(header, stream) {
     this.offset = 0;
-    
+
     this.header = header;
     this.stream = stream;
-    
+
     this.decoders = {
         /* Identification Frames */
         'TIT1': decodeTextFrame,
@@ -256,7 +256,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TPOS': decodeTextFrame,
         'TSST': decodeTextFrame,
         'TSRC': decodeTextFrame,
-        
+
         /* Involved Persons Frames */
         'TPE1': decodeTextFrame,
         'TPE2': decodeTextFrame,
@@ -269,7 +269,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TMCL': decodeTextFrame,
         'TIPL': decodeTextFrame,
         'TENC': decodeTextFrame,
-        
+
         /* Derived and Subjective Properties Frames */
         'TBPM': decodeTextFrame,
         'TLEN': decodeTextFrame,
@@ -279,7 +279,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TFLT': decodeTextFrame,
         'TMED': decodeTextFrame,
         'TMOO': decodeTextFrame,
-        
+
         /* Rights and Licence Frames */
         'TCOP': decodeTextFrame,
         'TPRO': decodeTextFrame,
@@ -287,7 +287,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TOWN': decodeTextFrame,
         'TRSN': decodeTextFrame,
         'TRSO': decodeTextFrame,
-        
+
         /* Other Text Frames */
         'TOFN': decodeTextFrame,
         'TDLY': decodeTextFrame,
@@ -300,25 +300,25 @@ Mad.ID3v23Stream = function(header, stream) {
         'TSOA': decodeTextFrame,
         'TSOP': decodeTextFrame,
         'TSOT': decodeTextFrame,
-        
+
         /* Attached Picture Frame */
         'APIC': decodeAttachedPictureFrame,
-        
+
         /* Unique Identifier Frame */
         'UFID': decodeIdentifierFrame,
-        
+
         /* Music CD Identifier Frame */
         'MCDI': decodeBinaryFrame,
-        
+
         /* Comment Frame */
         'COMM': decodeCommentFrame,
-        
+
         /* User Defined URL Link Frame */
         'WXXX': decodeUserDefinedLinkFrame,
-        
+
         /* Private Frame */
         'PRIV': decodeBinaryFrame,
-        
+
         /* Deprecated ID3v2 Frames */
         'TDAT': decodeTextFrame,
         'TIME': decodeTextFrame,
@@ -326,11 +326,11 @@ Mad.ID3v23Stream = function(header, stream) {
         'TRDA': decodeTextFrame,
         'TSIZ': decodeTextFrame,
         'TYER': decodeTextFrame,
-        
+
         /* General encapsulated object */
         'GEOB': decodeTerminatedString
     };
-    
+
     this.names = {
         /* Identification Frames */
         'TIT1': 'Content group description',
@@ -342,7 +342,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TPOS': 'Part of a set',
         'TSST': 'Set subtitle',
         'TSRC': 'ISRC',
-        
+
         /* Involved Persons Frames */
         'TPE1': 'Lead artist/Lead performer/Soloist/Performing group',
         'TPE2': 'Band/Orchestra/Accompaniment',
@@ -355,7 +355,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TMCL': 'Musician credits list',
         'TIPL': 'Involved people list',
         'TENC': 'Encoded by',
-        
+
         /* Derived and Subjective Properties Frames */
         'TBPM': 'BPM',
         'TLEN': 'Length',
@@ -365,7 +365,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TFLT': 'File type',
         'TMED': 'Media type',
         'TMOO': 'Mood',
-        
+
         /* Rights and Licence Frames */
         'TCOP': 'Copyright message',
         'TPRO': 'Produced notice',
@@ -373,7 +373,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TOWN': 'File owner/licensee',
         'TRSN': 'Internet radio station name',
         'TRSO': 'Internet radio station owner',
-        
+
         /* Other Text Frames */
         'TOFN': 'Original filename',
         'TDLY': 'Playlist delay',
@@ -386,25 +386,25 @@ Mad.ID3v23Stream = function(header, stream) {
         'TSOA': 'Album sort order',
         'TSOP': 'Performer sort order',
         'TSOT': 'Title sort order',
-        
+
         /* Attached Picture Frame */
         'APIC': 'Attached picture',
-        
+
         /* Unique Identifier Frame */
         'UFID': 'Unique identifier',
-        
+
         /* Music CD Identifier Frame */
         'MCDI': 'Music CD identifier',
-        
+
         /* Comment Frame */
         'COMM': 'Comment',
-        
+
         /* User Defined URL Link Frame */
         'WXXX': 'User defined URL link',
-        
+
         /* Private Frame */
         'PRIV': 'Private',
-        
+
         /* Deprecated ID3v2 frames */
         'TDAT': 'Date',
         'TIME': 'Time',
@@ -413,7 +413,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TSIZ': 'Size',
         'TYER': 'Year'
     };
-    
+
     this.extenders = {
         /* Identification Frames */
         'TIT1': extendWithValueFrame,
@@ -425,7 +425,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TPOS': extendWithValueFrame,
         'TSST': extendWithValueFrame,
         'TSRC': extendWithValueFrame,
-        
+
         /* Involved Persons Frames */
         'TPE1': extendWithValueFrame,
         'TPE2': extendWithValueFrame,
@@ -438,7 +438,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TMCL': extendWithValueFrame,
         'TIPL': extendWithValueFrame,
         'TENC': extendWithValueFrame,
-        
+
         /* Derived and Subjective Properties Frames */
         'TBPM': extendWithValueFrame,
         'TLEN': extendWithValueFrame,
@@ -448,7 +448,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TFLT': extendWithValueFrame,
         'TMED': extendWithValueFrame,
         'TMOO': extendWithValueFrame,
-        
+
         /* Rights and Licence Frames */
         'TCOP': extendWithValueFrame,
         'TPRO': extendWithValueFrame,
@@ -456,7 +456,7 @@ Mad.ID3v23Stream = function(header, stream) {
         'TOWN': extendWithValueFrame,
         'TRSN': extendWithValueFrame,
         'TRSO': extendWithValueFrame,
-        
+
         /* Other Text Frames */
         'TOFN': extendWithValueFrame,
         'TDLY': extendWithValueFrame,
@@ -469,25 +469,25 @@ Mad.ID3v23Stream = function(header, stream) {
         'TSOA': extendWithValueFrame,
         'TSOP': extendWithValueFrame,
         'TSOT': extendWithValueFrame,
-        
+
         /* Attached Picture Frame */
         'APIC': extendWithPictureFrame,
-        
+
         /* Unique Identifier Frame */
         'UFID': extendWithIdentifierFrame,
-        
+
         /* Music CD Identifier */
         'MCDI': extendWithValueFrame,
-        
+
         /* Comment Frame */
         'COMM': extendWithCommentFrame,
-        
+
         /* User Defined URL Link Frame */
         'WXXX': extendWithUserDefinedLinkFrame,
-        
+
         /* Private Frame */
         'PRIV': extendWithPrivateFrame,
-        
+
         /* Deprecated ID3v2 Frames */
         'TDAT': extendWithValueFrame,
         'TIME': extendWithValueFrame,
@@ -502,25 +502,25 @@ Mad.ID3v23Stream.prototype.readFrame = function() {
     if (this.offset >= this.header.length) {
         return null;
     }
-    
+
     var identifier = this.stream.readAsString(4);
-    
+
     if (identifier.charCodeAt(0) === 0) {
         this.offset = this.header.length + 1;
         return null;
     }
-    
+
     var length = this.stream.readU32(true);
     var flags = this.stream.readU16(true);
-    
+
     var header = {
         'identifier': identifier,
         'length': length,
         'flags': flags
     };
-    
+
     var result = null;
-    
+
     if (this.decoders[identifier]) {
         result = this.decoders[identifier](header, this.stream);
     } else {
@@ -528,52 +528,52 @@ Mad.ID3v23Stream.prototype.readFrame = function() {
             'identifier': identifier,
             'header': header
         };
-        
+
         this.stream.readAsString(Math.min(length, this.header.length - this.offset));
-	}
-    
-    if(result) {
-		result['name'] = this.names[identifier] ? this.names[identifier] : 'UNKNOWN';
     }
-    
+
+    if(result) {
+        result['name'] = this.names[identifier] ? this.names[identifier] : 'UNKNOWN';
+    }
+
     this.offset += 10 + length;
-    
+
     return result;
 }
 
 Mad.ID3v23Stream.prototype.read = function() {
     if (!this.array) {
         this.array = [];
-        
+
         var frame = null;
-        
+
         //try {
-            while (frame = this.readFrame()) {
-                this.array.push(frame);
-            }
+        while (frame = this.readFrame()) {
+            this.array.push(frame);
+        }
         //} catch (e) {
-            //throw(e);
-            //console.log("ID3 Error: " + e);
+        //throw(e);
+        //console.log("ID3 Error: " + e);
         //}
     }
-    
+
     return this.array;
 }
 
 Mad.ID3v23Stream.prototype.toHash = function() {
     var frames = this.read();
-    
+
     var hash = {};
-    
+
     for (var i = 0; i < frames.length; i++) {
         var frame = frames[i];
-        
+
         var extender = this.extenders[frame['header']['identifier']];
-        
+
         if (extender) {
             hash = extender(hash, frames[i]);
         }
     }
-    
+
     return hash;
 }
--- src/player.js
+++ src/player.js
@@ -15,117 +15,117 @@ Mad.Player = function (stream) {
 
 // Create a device.
 Mad.Player.prototype.createDevice = function() {
-	var synth = new Mad.Synth();
-	this.frame = new Mad.Frame();
-	this.frame = Mad.Frame.decode(this.frame, this.mpeg);
-	if (this.frame === null) {
-		if (this.mpeg.error === Mad.Error.BUFLEN) {
-			console.log("End of file!");
-		}
+    var synth = new Mad.Synth();
+    this.frame = new Mad.Frame();
+    this.frame = Mad.Frame.decode(this.frame, this.mpeg);
+    if (this.frame === null) {
+        if (this.mpeg.error === Mad.Error.BUFLEN) {
+            console.log("End of file!");
+        }
 
-		console.log("First error! code = " + this.mpeg.error + ", recoverable ? = " + Mad.recoverable(this.mpeg.error));
-		return;
-	}
+        console.log("First error! code = " + this.mpeg.error + ", recoverable ? = " + Mad.recoverable(this.mpeg.error));
+        return;
+    }
 
-	this.channelCount = this.frame.header.nchannels();
-	this.sampleRate = this.frame.header.samplerate;
+    this.channelCount = this.frame.header.nchannels();
+    this.sampleRate = this.frame.header.samplerate;
 
-	console.log("this.playing " + this.channelCount + " channels, samplerate = " + this.sampleRate + " audio, mode " + this.frame.header.mode);
+    console.log("this.playing " + this.channelCount + " channels, samplerate = " + this.sampleRate + " audio, mode " + this.frame.header.mode);
 
-	this.offset = 0;
-	this.absoluteFrameIndex = 0;
-	synth.frame(this.frame);
-	
-	this.lastRebuffer = Date.now();
-	this.playing = false;
-	this.progress();
-	
-	var self = this;	
-	var MAX_FRAMES_IN_BUFFER = 40;
-	
-	this.refill = function (sampleBuffer) {
-		//console.log("delta = " + (Date.now() - self.lastRebuffer) + ", asked for " + sampleBuffer.length);
-		self.lastRebuffer = Date.now();
-		
-		if(!self.playing) return; // empty sampleBuffer, no prob
-		
-		var index = 0;
+    this.offset = 0;
+    this.absoluteFrameIndex = 0;
+    synth.frame(this.frame);
 
-		while (index < sampleBuffer.length) {
-			for (var i = 0; i < self.channelCount; ++i) {
-				sampleBuffer[index++] = synth.pcm.samples[i][self.offset];
-			}
+    this.lastRebuffer = Date.now();
+    this.playing = false;
+    this.progress();
 
-			self.offset++;
-			
-			if (self.offset >= synth.pcm.samples[0].length) {
-				self.offset = 0;
+    var self = this;	
+    var MAX_FRAMES_IN_BUFFER = 40;
 
-				self.frame = Mad.Frame.decode(self.frame, self.mpeg);
-				if (self.frame === null) {
-					if (self.stream.error === Mad.Error.BUFLEN) {
-						console.log("End of file!");
-					}
-					console.log("Error! code = " + self.mpeg.error);
-					self.playing = false;
-					self.onProgress(1.0, 1.0, 1.0);
-					self.dev.kill();
-				} else {
-					synth.frame(self.frame);
-					self.absoluteFrameIndex++;
-				}
-			}
-		}
+    this.refill = function (sampleBuffer) {
+        //console.log("delta = " + (Date.now() - self.lastRebuffer) + ", asked for " + sampleBuffer.length);
+        self.lastRebuffer = Date.now();
 
-		if (self.onPostProcessing) {
-			self.onPostProcessing.apply(this, arguments);
-		}
+        if(!self.playing) return; // empty sampleBuffer, no prob
 
-	};
-	
-	this.reinitDevice();
+        var index = 0;
+
+        while (index < sampleBuffer.length) {
+            for (var i = 0; i < self.channelCount; ++i) {
+                sampleBuffer[index++] = synth.pcm.samples[i][self.offset];
+            }
+
+            self.offset++;
+
+            if (self.offset >= synth.pcm.samples[0].length) {
+                self.offset = 0;
+
+                self.frame = Mad.Frame.decode(self.frame, self.mpeg);
+                if (self.frame === null) {
+                    if (self.stream.error === Mad.Error.BUFLEN) {
+                        console.log("End of file!");
+                    }
+                    console.log("Error! code = " + self.mpeg.error);
+                    self.playing = false;
+                    self.onProgress(1.0, 1.0, 1.0);
+                    self.dev.kill();
+                } else {
+                    synth.frame(self.frame);
+                    self.absoluteFrameIndex++;
+                }
+            }
+        }
+
+        if (self.onPostProcessing) {
+            self.onPostProcessing.apply(this, arguments);
+        }
+
+    };
+
+    this.reinitDevice();
 };
 
 Mad.Player.prototype.reinitDevice = function() {
-	if(this.dev) this.dev.kill();
-	var preBufferSize = 65536 * 4096;
-	var self = this;
-	this.dev = Sink(function(){
-		return self.refill.apply(this, arguments);
-	}, this.channelCount, preBufferSize, this.sampleRate);
+    if(this.dev) this.dev.kill();
+    var preBufferSize = 65536 * 4096;
+    var self = this;
+    this.dev = Sink(function(){
+            return self.refill.apply(this, arguments);
+            }, this.channelCount, preBufferSize, this.sampleRate);
 }
 
 Mad.Player.prototype.setPlaying = function(playing) {
-	this.playing = playing;
-	if(playing) {
-		this.onPlay();
-	} else {
-		this.onPause();
-	}
+    this.playing = playing;
+    if(playing) {
+        this.onPlay();
+    } else {
+        this.onPause();
+    }
 }
 
 Mad.Player.prototype.destroy = function() {
-	clearTimeout(this.progressTimeout);
-	if(this.dev) {
-		this.dev.kill();
-	}
+    clearTimeout(this.progressTimeout);
+    if(this.dev) {
+        this.dev.kill();
+    }
 }
 
 Mad.Player.prototype.progress = function () {
-	var delta = Date.now() - this.lastRebuffer;
-	if(delta > 1000) {
-		// freaking Firefox sometimes fails at scheduling tasks and we lose audio streaming
-		console.log("Device reinit - buffer underflow.");
-		this.reinitDevice();
-	}
-	
+    var delta = Date.now() - this.lastRebuffer;
+    if(delta > 1000) {
+        // freaking Firefox sometimes fails at scheduling tasks and we lose audio streaming
+        console.log("Device reinit - buffer underflow.");
+        this.reinitDevice();
+    }
+
     var playtime = ((this.absoluteFrameIndex * 1152 + this.offset) / this.sampleRate) + delta / 1000.0;
     //console.log("delta = " + delta + ", contentLength = " + this.stream.contentLength + ", this.offset = " + this.mpeg.this_frame);
     var total = this.totalLength ? this.totalLength : playtime * this.stream.contentLength / this.mpeg.this_frame;
     var preloaded = this.stream.amountRead / this.stream.contentLength;
     //console.log("amountRead = " + this.stream.amountRead + ", preloaded = " + preloaded);
     this.onProgress(playtime, total, preloaded);
-    
+
     var that = this;
     var nextCall = function() { that.progress(); };
     this.progressTimeout = setTimeout(nextCall, 250);
@@ -133,13 +133,13 @@ Mad.Player.prototype.progress = function () {
 
 Mad.Player.fromFile = function (file, callback) {
     new Mad.FileStream(file, function (stream) {
-        callback(new Mad.Player(stream));
-    });
+            callback(new Mad.Player(stream));
+            });
 };
 
 Mad.Player.fromURL = function (url, callback) {
     var stream = new Mad.AjaxStream(url);
     stream.requestAbsolute(1 * 1024, function () {
-        callback(new Mad.Player(stream));
-    });
+            callback(new Mad.Player(stream));
+            });
 };
